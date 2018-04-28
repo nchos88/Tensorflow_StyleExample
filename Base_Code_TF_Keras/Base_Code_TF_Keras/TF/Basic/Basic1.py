72c57e9385da29b95605e309bd76fdb08637e70e
pimier15 <pimier15@gmail.com>
2018-04-28 12:38:54 +0900

    update

--- Base_Code_TF_Keras/Base_Code_TF_Keras/TF/Basic/Basic1.py
+++ Base_Code_TF_Keras/Base_Code_TF_Keras/TF/Basic/Basic1.py
@@ -1,1 +1,29 @@
+import tensorflow as tf
+
+x = [[1,2,3], [4,5,6]]
+
+X = tf.placeholder(tf.float32 , [None , 3])
+
+
+W = tf.Variable(tf.random_normal([3,2]))
+b = tf.Variable(tf.random_normal([2,1]))
+
+w_init_data = [[0.1,0.1],[0.3,0.4],[0.1,0.4]]
+W_initialized = tf.Variable( w_init_data )
+
+res = tf.matmul(X,W) + b
+
+
+# runner
+sess = tf.Session()
+sess.run(tf.global_variable_initializer())
+print('-- x --')
+print(sess.run(X))
+print('-- w --')
+print(sess.run(W))
+print('-- b --')
+print(sess.run(x))
+
+
+
 
